b74a87b9fb04367fde61632c6a84e5aa77bc3afd
MohamedATeama <mohammed.teama789@gmail.com>
2026-03-03 10:34:07 +0200

    authorization meddleware

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -2,6 +2,9 @@ import expressAsyncHandler from "express-async-handler";
 import { loginService, registerService, verifyRefreshToken } from "./auth.service.js";
 import { createResponse, successResponse } from "../../utils/APIResponse.js";
 
+// @desc     Register
+// @route    POST /auth/register
+// @access   public
 export const register = expressAsyncHandler(async (req, res) => {
   const {user, accessToken, refreshToken} = await registerService(req.body);
 
@@ -22,6 +25,9 @@ export const register = expressAsyncHandler(async (req, res) => {
   res.status(201).json(createResponse({user, token: {accessToken, refreshToken}}, "Register successfully"));
 });
 
+// @desc     Login
+// @route    POST /auth/login
+// @access   public
 export const login = expressAsyncHandler(async (req, res) => {
   const {user, accessToken, refreshToken} = await loginService(req.body);
 
@@ -42,6 +48,9 @@ export const login = expressAsyncHandler(async (req, res) => {
   res.status(200).json(successResponse({user, token: {accessToken, refreshToken}}, "Login successfully"));
 });
 
+// @desc     Create access token
+// @route    POST /auth/refresh
+// @access   Private
 export const createAccessToken = expressAsyncHandler(async (req, res) => {
   const refreshToken = req.cookies?.refreshToken || req.body.refreshToken;
   const accessToken = await verifyRefreshToken(refreshToken);
@@ -55,6 +64,9 @@ export const createAccessToken = expressAsyncHandler(async (req, res) => {
   res.status(200).json(successResponse(accessToken, "Access token created successfully"));
 });
 
+// @desc     Logout
+// @route    POST /auth/logout
+// @access   Private
 export const logout = expressAsyncHandler(async (req, res) => {
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
