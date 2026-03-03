eb2999b4912691100adb77666a623da356a6c0b0
MohamedATeama <mohammed.teama789@gmail.com>
2026-03-03 21:59:32 +0200

    register validation, login validation, updateProfile validation and change password validation

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,42 +1,43 @@
-import { createValidationError } from "../utils/APIErrors.js";
+import { createValidationError } from "../utils/APIErrors.js"
 
-export const ValidationRequest = (schema) => {
+const Validate = (schema) => {
   return async (req, res, next) => {
     try {
-      const { value, error } = schema.validate(
-        {
-          body: req.body,
-          params: req.params,
-          query: req.query,
-          file: req.file,
-          files: req.files,
-        },
+      const { value, error } = schema.validate({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        file: req.file,
+        files: req.files
+      },
         {
           abortEarly: false,
-          stripUnknown: true,
+          stripUnknown: true
         }
-      );
+      )
 
       if (error) {
-        const errorDetail = error.details.map((detail) => ({
-          field: detail.path.join("."),
-          message: detail.message,
-        }));
-
-        const validationError = createValidationError("validation failed");
-        validationError.details = errorDetail;
-        throw validationError;
+        const errorDetail = error.details.map(detail => (
+          {
+            field: detail.path.join("."),
+            message: detail.message
+          }
+        ))
+        const validationError = createValidationError("validation failed")
+        validationError.stack = errorDetail
+        throw validationError
       }
 
-      req.validated = value;
+      req.validated = value
+      if (value.body) req.body = value.body
+      if (value.params) req.params = value.params
+      if (value.query) req.query = value.query
 
-      if (value.body) req.body = value.body;
-      if (value.params) req.params = value.params;
-      if (value.query) req.query = value.query;
-
-      next();
+      next()
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
-};
+  }
+}
+
+export default Validate;
--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { changeUserPassword, createAccessToken, getCurrentUser, login, logout, register, updateProfile } from "./auth.controller.js";
 import { authentication } from "../../middlewares/authentication.js";
 import { authLimiter } from "../../middlewares/rateLimit.js";
-import { changePasswordValidator, loginValidator, registerValidator, updateProfileValidator } from "./auth.validator.js";
+import Validate from "../../middlewares/validate.js";
+import { changePasswordValidation, loginValidation, registerValidation, updateProfileValidation } from "./auth.validator.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", authLimiter, registerValidator, register);
-authRouter.post("/login", authLimiter, loginValidator, login);
+authRouter.post("/register", authLimiter, Validate(registerValidation), register);
+authRouter.post("/login", authLimiter, Validate(loginValidation), login);
 authRouter.post('/refresh', authentication, createAccessToken);
 authRouter.post('/logout', authentication, logout);
 authRouter.get('/me', authentication, getCurrentUser);
-authRouter.put('/updateProfile', authentication, updateProfileValidator, updateProfile);
-authRouter.put('/changePassword', authentication, changePasswordValidator, changeUserPassword);
+authRouter.put('/updateProfile', authentication, Validate(updateProfileValidation), updateProfile);
+authRouter.put('/changePassword', authentication, Validate(changePasswordValidation), changeUserPassword);
 
 export default authRouter;
