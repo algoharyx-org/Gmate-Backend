de5f479b58d85ace61a81b9626a60eaee0c225c0
MohamedATeama <mohammed.teama789@gmail.com>
2026-03-03 21:04:02 +0200

    validation middleware

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,15 +2,16 @@ import { Router } from "express";
 import { changeUserPassword, createAccessToken, getCurrentUser, login, logout, register, updateProfile } from "./auth.controller.js";
 import { authentication } from "../../middlewares/authentication.js";
 import { authLimiter } from "../../middlewares/rateLimit.js";
+import { changePasswordValidator, loginValidator, registerValidator, updateProfileValidator } from "./auth.validator.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", authLimiter, register);
-authRouter.post("/login", authLimiter, login);
+authRouter.post("/register", authLimiter, registerValidator, register);
+authRouter.post("/login", authLimiter, loginValidator, login);
 authRouter.post('/refresh', authentication, createAccessToken);
 authRouter.post('/logout', authentication, logout);
 authRouter.get('/me', authentication, getCurrentUser);
-authRouter.put('/updateProfile', authentication, updateProfile);
-authRouter.put('/changePassword', authentication, changeUserPassword);
+authRouter.put('/updateProfile', authentication, updateProfileValidator, updateProfile);
+authRouter.put('/changePassword', authentication, changePasswordValidator, changeUserPassword);
 
 export default authRouter;
