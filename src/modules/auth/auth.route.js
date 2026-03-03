0105594cf03eb190242d8ac52989cb11a323bf62
MohamedATeama <mohammed.teama789@gmail.com>
2026-03-03 10:54:38 +0200

    rate limit middleware

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createAccessToken, login, logout, register } from "./auth.controller.js";
 import { authentication } from "../../middlewares/authentication.js";
+import { authLimiter } from "../../middlewares/rateLimit.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
+authRouter.post("/register", authLimiter, register);
+authRouter.post("/login", authLimiter, login);
 authRouter.post('/refresh', authentication, createAccessToken);
 authRouter.post('/logout', authentication, logout);
+
 export default authRouter;
