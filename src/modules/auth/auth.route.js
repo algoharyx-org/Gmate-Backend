f6a112e63cd53005c9d4abba8da884fa538955b4
MohamedATeama <mohammed.teama789@gmail.com>
2026-03-03 10:20:36 +0200

    authentication middleware

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createAccessToken, login, logout, register } from "./auth.controller.js";
+import { authentication } from "../../middlewares/authentication.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
-authRouter.post('/refresh', createAccessToken);
-authRouter.post('/logout', logout);
+authRouter.post('/refresh', authentication, createAccessToken);
+authRouter.post('/logout', authentication, logout);
 export default authRouter;
